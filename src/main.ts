b466f261f573d6d22835f14552f0447c722d15e0
Felyppe Nunes <felyppe.nunes1@gmail.com>
2025-03-03 23:44:12 -0300

    refactor: main file in a serverless function way

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import 'dotenv/config'
 
-import { ExpressServer } from './infrastructure/servers/express/server'
-import { FastifyServer } from './infrastructure/servers/fastify/server'
+import Fastify from 'fastify'
+import fastifyCookie from 'fastify-cookie'
+import { routes } from './infrastructure/servers/fastify/routes'
+import { InvalidDataError } from './domain/errors/invalid-data-error'
+import { NotFoundError } from './domain/errors/not-found-error'
+import { ConflictError } from './domain/errors/conflict-error'
 
-// const expressServer = new ExpressServer()
-const fastifyServer = new FastifyServer()
+const app = Fastify()
 
-// expressServer.startServer(3333)
-fastifyServer.startServer(4444)
+app.register(fastifyCookie)
+
+app.register(routes)
+
+app.setErrorHandler((error, request, reply) => {
+    if (error instanceof InvalidDataError) {
+        return reply.status(400).send({ error: error.message })
+    }
+
+    if (error instanceof NotFoundError) {
+        return reply.status(404).send({ error: error.message })
+    }
+
+    if (error instanceof ConflictError) {
+        return reply.status(409).send({ error: error.message })
+    }
+
+    return reply.status(500).send({ error: error.message })
+})
+
+export default async (req: any, res: any) => {
+    await app.ready()
+    app.server.emit('request', req, res)
+}
