7e2439b4fd250cfa2c02ee66b3373d9a3ec8bbba
Felyppe Nunes <felyppe.nunes1@gmail.com>
2025-05-29 00:48:11 -0300

    refactor: max instances

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,7 +4,7 @@ resource "google_vpc_access_connector" "vpc_connector" {
   network       = "default"
   ip_cidr_range = "10.8.0.0/28"
   min_instances = 2
-  max_instances = 2
+  max_instances = 3
 
   depends_on = [
     google_project_service.required_apis["vpcaccess.googleapis.com"]
