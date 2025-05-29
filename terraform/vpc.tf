599b5cc5a4a9820a6b92267864ae37283255550a
Felyppe Nunes <felyppe.nunes1@gmail.com>
2025-05-29 00:45:09 -0300

    fix: vpc connector

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,6 +3,7 @@ resource "google_vpc_access_connector" "vpc_connector" {
   region        = var.region
   network       = "default"
   ip_cidr_range = "10.8.0.0/28"
+  max_instances = 2
 
   depends_on = [
     google_project_service.required_apis["vpcaccess.googleapis.com"]
