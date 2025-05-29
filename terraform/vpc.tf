f7455e5ed726a09acb527a015559e8a14db1ae71
Felyppe Nunes <felyppe.nunes1@gmail.com>
2025-05-29 00:46:35 -0300

    fix: min instances

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,6 +3,7 @@ resource "google_vpc_access_connector" "vpc_connector" {
   region        = var.region
   network       = "default"
   ip_cidr_range = "10.8.0.0/28"
+  min_instances = 2
   max_instances = 2
 
   depends_on = [
