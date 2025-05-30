113fca41729de93ab32acf27b75c042ac9caaf32
Felyppe Nunes <felyppe.nunes1@gmail.com>
2025-05-29 22:37:42 -0300

    feat: router and nat

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,3 +10,24 @@ resource "google_vpc_access_connector" "vpc_connector" {
     google_project_service.required_apis["vpcaccess.googleapis.com"]
   ]
 }
+
+resource "google_compute_router" "cloud_router" {
+  name    = "default-router"
+  network = "default"
+  region  = var.region 
+  project = var.project
+}
+
+resource "google_compute_router_nat" "cloud_nat" {
+  name                               = "default-nat"
+  router                             = google_compute_router.cloud_router.name
+  region                             = var.region
+  project                            = var.project
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = false
+    filter = "ERRORS_ONLY"
+  }
+}
